2f202ed30a70d6dcd0f8f856eec9a1b7564d9ff9
白浩然 <bai_haoran@outlook.com>
2017-05-07 10:39:35 +0800

    添加了一些对类和方法说明的注释

--- src/com/daoshengwanwu/math_util/calculator/ExpItem.java
+++ src/com/daoshengwanwu/math_util/calculator/ExpItem.java
@@ -1,8 +1,12 @@
 package com.daoshengwanwu.math_util.calculator;
 
 
+/*
+ * 构成表达式的项：构成AriExp以及VarAriExp的基本单位
+ */
 abstract class ExpItem {
-	private ItemType mItemType;
+	//该项的类型，取值分别有：OPERATOR(运算符)、OPERAND(操作数)、VARIABLE(变量)
+	private final ItemType mItemType;
 	
 	
 	ExpItem(ItemType itemType) {
@@ -14,23 +18,35 @@ abstract class ExpItem {
 	}//getItemType
 	
 	
+	/*
+	 * 项的类型的枚举定义
+	 */
 	static enum ItemType {
 		OPERATOR, OPERAND, VARIABLE;
 	}//enum_ItemType	
 	
 	
+	/*
+	 * 运算符类
+	 */
 	static class Operator extends ExpItem {
 		Operator() {
 			super(ItemType.OPERATOR);
 		}//con_Operator		
 	}//class_Operator
 	
+	/*
+	 * 操作数类
+	 */
 	static class Operand extends ExpItem {
 		Operand() {
 			super(ItemType.OPERAND);
 		}//con_Operand
 	}//class_Operand
 	
+	/*
+	 * 变量类
+	 */
 	static class Variable extends ExpItem {
 		Variable() {
 			super(ItemType.VARIABLE);
